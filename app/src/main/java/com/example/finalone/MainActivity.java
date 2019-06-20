461d09ffc1c321f651d7a7e4ddb8e049d10da442
saif04 <saif.amairi7@gmail.com>
2019-06-20 16:12:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/finalone/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.finalone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static android.R.layout.simple_spinner_item;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String URLstring = "https://api.myjson.com/bins/vzdjp";
+    private static ProgressDialog mProgressDialog;
+    private ArrayList<GoodModel> goodModelArrayList;
+    private ArrayList<String> names = new ArrayList<String>();
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spCompany);
+
+        retrieveJSON();
+
+    }
+
+    private void retrieveJSON() {
+
+        showSimpleProgressDialog(this, "Loading...","Fetching Json",false);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLstring,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("strrrrr", ">>" + response);
+
+                        try {
+
+                           // JSONObject obj = new JSONObject(response);
+
+
+                                goodModelArrayList = new ArrayList<>();
+                                JSONArray dataArray  = new JSONArray(response);// response chaine de caractre kifach bach in7otha fi json array
+
+                                for (int i = 0; i < dataArray.length(); i++) {
+
+                                    GoodModel playerModel = new GoodModel();
+                                    JSONObject dataobj = dataArray.getJSONObject(i);
+
+                                    playerModel.setName(dataobj.getString("title"));
+                                    playerModel.setId(Integer.parseInt((dataobj.getString ("id"))));
+
+                                   // playerModel.setCity(dataobj.getString("city"));
+                                    //playerModel.setImgURL(dataobj.getString("imgURL"));
+
+                                    goodModelArrayList.add(playerModel);
+
+                                }
+
+                                for (int i = 0; i < goodModelArrayList.size(); i++){
+                                    names.add(goodModelArrayList.get(i).getTitle().toString());
+                                }
+
+                                ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(MainActivity.this, simple_spinner_item, names);
+                                spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+                                spinner.setAdapter(spinnerArrayAdapter);
+                                removeSimpleProgressDialog();
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //displaying the error in toast if occurrs
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        requestQueue.add(stringRequest);
+
+
+    }
+
+    public static void removeSimpleProgressDialog() {
+        try {
+            if (mProgressDialog != null) {
+                if (mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
+                    mProgressDialog = null;
+                }
+            }
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void showSimpleProgressDialog(Context context, String title,
+                                                String msg, boolean isCancelable) {
+        try {
+            if (mProgressDialog == null) {
+                mProgressDialog = ProgressDialog.show(context, title, msg);
+                mProgressDialog.setCancelable(isCancelable);
+            }
+
+            if (!mProgressDialog.isShowing()) {
+                mProgressDialog.show();
+            }
+
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
